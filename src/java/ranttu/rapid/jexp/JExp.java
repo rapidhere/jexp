6c43c8f5a218446eb44a96acc29c1bac4dd877cc
dongwei.dq <dongwei.dq@alibaba-inc.com>
2017-07-27 19:43:03 +0800

    add gradle support

---
+++ src/java/ranttu/rapid/jexp/JExp.java
@@ -0,0 +1,10 @@
+package ranttu.rapid.jexp;
+
+/**
+ * The jExp facade
+ * @author rapidhere@gmail.com
+ * @version $Id: JExp.java, v0.1 2017-07-27 7:41 PM dongwei.dq Exp $
+ */
+final public class JExp {
+    private JExp() {}
+}
